0f592604ae720ad2f5adff1f0ed61884f7ffc48c
Ankit Kumar <ankit.official.dev@gmail.com>
2020-06-03 16:52:19 +0530

    Main window

---
+++ app.js
@@ -0,0 +1,28 @@
+
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron')
+
+
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 750,
+    height: 500,
+    webPreferences: {
+     nodeIntegration : true,
+     nodeIntegrationInWorker : true,
+    },
+    frame : false,
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('./gui/index.html')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+}
+
+app.whenReady().then( () => {
+  createWindow()
+
+})
