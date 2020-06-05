e3161250aa046c79701a07423170d275da8e6598
Ankit Kumar <ankit.official.dev@gmail.com>
2020-06-06 01:56:56 +0530

    Added IPC to python backend

--- behaviour.js
+++ behaviour.js
@@ -1,6 +1,5 @@
 const remote = require("electron").remote;
 
-
 function minimize() {
   var window = remote.getCurrentWindow();
   window.minimize();
@@ -9,4 +8,14 @@ function minimize() {
 function close_win(){
     var window = remote.getCurrentWindow();
     window.close();
+}
+
+function remotePythonExecution(){
+    searchUsername = document.getElementById("username").value;
+    console.log(searchUsername);
+    const {PythonShell} = require("python-shell");
+    var shell = new PythonShell('backend.py', { mode: 'text', args: [searchUsername] });
+    shell.on('message', function (message) {
+     console.table(message);
+    });
 }
