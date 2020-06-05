2efab129957aa6d410b0d8b9f2ccd1ce005779e3
Ankit Kumar <ankit.official.dev@gmail.com>
2020-06-05 23:26:46 +0530

    input as a system arg

--- backend.py
+++ backend.py
@@ -1,8 +1,8 @@
-import requests, json
+import requests, json, sys
 
-ip = input("User ID \n")
+username = sys.argv[1]
 
-response = requests.get('https://www.instagram.com/{}/?__a=1'.format(ip))
+response = requests.get('https://www.instagram.com/{}/?__a=1'.format(username))
 raw_data = response.content.decode("utf-8") 
 raw_data = json.loads(raw_data)
 graph_request = raw_data['graphql']
@@ -10,12 +10,20 @@ user_content = graph_request['user']
 
 # User Contents
 
-name = user_content['full_name']
-bio = user_content['biography']
-followers = user_content['edge_followed_by']['count']
-follows = user_content['edge_follow']['count']
-profile_hd = user_content['profile_pic_url_hd']
-det = [name, bio, followers, follows]
-print(profile_hd)
+# name = user_content['full_name']
+# bio = user_content['biography']
+# followers = user_content['edge_followed_by']['count']
+# follows = user_content['edge_follow']['count']
+# profile_hd = user_content['profile_pic_url_hd']
+# det = [name, bio, followers, follows]
+# print(profile_hd)
 
-print(det)
+user_data_dict = {
+    'name' : user_content['full_name'],
+    'bio' : user_content['biography'],
+    'followers' : user_content['edge_followed_by']['count'],
+    'follows' : user_content['edge_follow']['count'],
+    'profile_hd' : user_content['profile_pic_url_hd'],
+}
+
+print(user_data_dict)
