a19e31fc188c5778a6285b08cc0cf921de333e33
Ankit Kumar <ankit.official.dev@gmail.com>
2020-06-07 01:41:47 +0530

    Added request module to directly parse data from IG graphQL

--- behaviour.js
+++ behaviour.js
@@ -5,17 +5,69 @@ function minimize() {
   window.minimize();
 }
 
-function close_win(){
-    var window = remote.getCurrentWindow();
-    window.close();
+function close_win() {
+  var window = remote.getCurrentWindow();
+  window.close();
 }
 
-function remotePythonExecution(){
-    searchUsername = document.getElementById("username").value;
-    console.log(searchUsername);
-    const {PythonShell} = require("python-shell");
-    var shell = new PythonShell('backend.py', { mode: 'text', args: [searchUsername] });
-    shell.on('message', function (message) {
-     console.table(message);
-    });
+function remotePythonExecution() {
+  if (!document.getElementById("userdetailplane").classList.contains("hide")) {
+    document.getElementById("userdetailplane").classList.add("hide");
+  }
+  document.getElementById("loaderimage").classList.remove("hide");
+  searchUsername = document.getElementById("username").value;
+  //   const { PythonShell } = require("python-shell");
+  //   var shell = new PythonShell("backend.py", {
+  //     mode: "text",
+  //     args: [searchUsername],
+  //   });
+  //   shell.on("message", function (message) {
+  //     console.log(message);
+  //     message = message.substr(1, message.length - 2);
+  //     user_data = message.split(",");
+
+  //     document.getElementById("loaderimage").classList.add("hide");
+  //     document.getElementById("userdetailplane").classList.remove("hide");
+
+  //     document.getElementById("usernamedesc").innerHTML = user_data[0];
+  //     if (user_data[1]) {
+  //       document.getElementById("userbio").innerHTML = user_data[1];
+  //     }
+  //     document.getElementById("userfollowers").innerHTML = user_data[2];
+  //     document.getElementById("userfollowedby").innerHTML = user_data[3];
+  //     document.getElementById("pimage").src = user_data[4];
+  //   });
+
+  var request = require("request");
+
+  request(
+    {
+      uri: "https://www.instagram.com/" + searchUsername + "/?__a=1",
+      method: "GET",
+      timeout: 10000,
+      followRedirect: false,
+    },
+    function (error, response, body) {
+      var raw_data = JSON.parse(body);
+      var user_details = raw_data.graphql.user;
+      var bio = user_details.biography;
+      var followers = user_details.edge_followed_by.cout;
+      var following = user_details.edge_follow.count;
+      var profileHD = user_details.profile_pic_url_hd;
+      var fullname = user_details.full_name;
+
+      console.log(fullname);
+      console.log(bio);
+
+      document.getElementById("loaderimage").classList.add("hide");
+      document.getElementById("userdetailplane").classList.remove("hide");
+      document.getElementById("usernamedesc").innerHTML = fullname;
+      if (bio) {
+        document.getElementById("userbio").innerHTML = bio;
+      }
+      document.getElementById("userfollowers").innerHTML = followers;
+      document.getElementById("userfollowedby").innerHTML = following;
+      document.getElementById("pimage").src = profileHD;
+    }
+  );
 }
